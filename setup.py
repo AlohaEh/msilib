361288e21e1d7427a93fc5108b5b66550c714756
AlohaEh <alohaeh@gmail.com>
2024-11-26 11:32:59 -0500

    Tweak import

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     name="pymsilib",
     version="1.0.0",
     description = 'msilib replacement',
+    long_description_content_type = 'text/x-rst',
     author = 'Trevor Hamm',
     author_email = 'alohaeh@gmail.com',
     ext_modules=[module],
