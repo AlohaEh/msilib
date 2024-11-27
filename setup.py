107f2949dcdf48295b738fcd49f4066be33600aa
AlohaEh <alohaeh@gmail.com>
2024-11-26 21:48:35 -0500

    removed duplicated info from setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,6 @@ module = Extension('pymsilib._msi',
 )
 
 setup(
-    name="pymsilib",
-    version="1.0.0",
-    description = 'msilib replacement',
-    long_description_content_type = 'text/x-rst',
-    author = 'Trevor Hamm',
-    author_email = 'alohaeh@gmail.com',
-    ext_modules=[module],
+    ext_modules=[module]
 )
 
